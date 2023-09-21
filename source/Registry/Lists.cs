a9b542144f5f3ff1b426b29b32efa2b26312d72b
Zontreck <tarapiccari@gmail.com>
2023-09-21 06:04:00 -0700

    Fix missing newline

--- source/Registry/Lists.cs
+++ source/Registry/Lists.cs
@@ -53,7 +53,7 @@ namespace TP.CS.Registry
             str += ind + "{\n";
             foreach(var item in Value)
             {
-                str += ind + $"\"{item}\",";
+                str += ind + $"\"{item}\",\n";
             }
             str = str.Trim().Trim(',');
             str += "\n" + ind + "}";
